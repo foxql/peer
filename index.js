639b60eea3ee294012ef30063a3ed460176aa35a
boraozer <nagayapim@gmail.com>
2021-12-16 06:11:04 +0300

    start pow signalling message implementation & added nodeId util method

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import 'regenerator-runtime/runtime'
 import bridge from './src/bridge'
 import signallingServer from './src/signalling'
 import sha256 from 'crypto-js/sha256'
+import { nodeId } from './src/utils'
+import { v4 as uuidv4 } from 'uuid'
 class p2pNetwork extends bridge{
     constructor({bridgeServer})
     {
         super(bridgeServer)
         this.signallingServers = {}
         this.events = {}
+        this.status = 'not-ready'
+        this.nodeId = nodeId()
     }
 
     start()
@@ -26,7 +30,9 @@ class p2pNetwork extends bridge{
             return false 
         }
 
-        this.signallingServers[key] = new signallingServer(host)
+        this.signallingServers[key] = new signallingServer(host, ()=> {
+            this.status = 'ready'
+        })
 
     }
 
@@ -41,6 +47,27 @@ class p2pNetwork extends bridge{
             this.events[listenerName] = listener
         });
     }
+
+    async pow({transportPackage, livingTime, ...params})
+    {
+        if(this.status !== 'ready') return {warning: this.status}
+
+        const tempListenerName = uuidv4()
+        
+        this.bridgeSocket.on(tempListenerName, data => { // listen transport event result
+            // todo p2p connection starting...
+        })
+
+        this.transportMessage({
+            ...transportPackage,
+            nodeId: this.nodeId,
+            answerPool: tempListenerName
+        })
+
+    
+    }
+
+
 
 }
 
