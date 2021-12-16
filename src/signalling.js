cfdb556ee1590fc73309e99e4bf114309ff4a56f
boraozer <nagayapim@gmail.com>
2021-12-16 06:10:18 +0300

    added connection callback

--- src/signalling.js
+++ src/signalling.js
@@ -1,11 +1,12 @@
 import io from 'socket.io-client'
 export default class {
 
-    constructor(host)
+    constructor(host, connectionCallback)
     {
         this.host = host
         this.signallingStatus = 'not-ready'
         this.signallingSocket = null
+        this.connectionCallback = connectionCallback
         this.connectSignallingServer()
     }
 
@@ -14,6 +15,7 @@ export default class {
         const socket = io(this.host)
         socket.on('connect', ()=> {
             this.signallingStatus = 'connected'
+            this.connectionCallback(this.host)
         })
         socket.on('disconnect', this.disconnectListener)
         this.signallingSocket = socket
