5c8f55875365c8130409ec58dbfa0defaa1de136
boraozer <nagayapim@gmail.com>
2022-01-16 19:57:14 +0300

    added maxCandidateCallTime

--- develop/client.js
+++ develop/client.js
@@ -5,7 +5,8 @@ const p2p = new network({
     bridgeServer: {
         host: 'http://127.0.0.1:1923'
     },
-    maxNodeCount: 30
+    maxNodeCount: 30,
+    maxCandidateCallTime: 2000 // ms
 });
 
 p2p.loadEvents(events)
