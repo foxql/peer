d1bac326a8f653530c03335c1f9088beedf35a67
boraozer <nagayapim@gmail.com>
2022-06-09 00:47:42 +0300

    hesaplanan hash değerinin varlığı kontrol edilecek şekilde değiştirildi

--- src/events/offer.js
+++ src/events/offer.js
@@ -3,7 +3,7 @@ export const listenerName = 'offer'
 export async function listener(data, simulate = false)
 {
     const {signature, from, sdp} = data
-    const access = this.sigStore.find(signature)
+    const access = this.sigStore.existWhiteList(signature)
 
     if(!access) return
     const offerNode = this.createNode(this.constantSignallingServer, from)
