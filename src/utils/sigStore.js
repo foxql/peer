03fea3e82fb29ef0d88899d34f10088d831b8de2
boraozer <nagayapim@gmail.com>
2022-06-09 00:47:07 +0300

    spam önleyebilmek için rastgele cevabı önceden bilinerek üretilen bir hash sorusu yaratıldı.

--- src/utils/sigStore.js
+++ src/utils/sigStore.js
@@ -1,21 +1,34 @@
 import { v4 as uuidv4 } from 'uuid'
+import sha256 from 'crypto-js/sha256'
 
 export default class {
 
     constructor(maxCount)
     {
+        this.maxNonce = 99999
+        this.minNonce = 1
+        this.letterCountForClue = 6
         this.maxSignatureCount = maxCount
         this.signatures = {}
+        
+        this.whiteList = {}
     }
 
-    generate(eventName, destroyTime)
+    generate(destroyTime)
     {
-        const key = uuidv4()
-        this.signatures[key] = eventName
+        const uuidKey = uuidv4()
+        const nonce = this.randomNonce()
+        const key = sha256(uuidKey + nonce).toString()
+        this.signatures[key] = nonce
         setTimeout(()=> {
             this.dropSignature(key)
         }, destroyTime)
-        return key
+        return {
+            question: {
+                uuid: uuidKey,
+                startWith: key.substring(0, this.letterCountForClue)
+            }
+        }
     }
 
     dropSignature(key)
@@ -26,6 +39,51 @@ export default class {
     find(key)
     {
         return this.signatures[key] || false
+    }
+
+    randomNonce()
+    {
+        return Math.floor(Math.random() * this.maxNonce) + this.minNonce
+    }
+
+    solveQuestion({uuid, startWith})
+    {
+        for(let nonce = this.minNonce; nonce <= this.maxNonce; nonce++){
+            const key = sha256(uuid + nonce).toString()
+            if(key.substring(0, this.letterCountForClue) == startWith){
+                this.whiteList[key] = nonce
+                this.dropWhiteList(key)
+                console.log({
+                    answer: key,
+                    nonce: nonce
+                })
+                return {
+                    answer: key,
+                    nonce: nonce
+                }
+            }
+        }
+        return false
+    }
+
+    isvalid({answer, nonce})
+    {
+        const findNonce = this.signatures[answer] || false
+        if(!findNonce) return false
+
+        return findNonce === nonce
+    }
+
+    dropWhiteList(key)
+    {
+        setTimeout(() => {
+            this.whiteList[key] = undefined
+        }, 1000)
+    }
+
+    existWhiteList(key)
+    {
+        return this.whiteList[key] || false
     }
 
 
