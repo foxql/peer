7949c02530376e9fe32499ff6602746a2db7aab5
boraozer <nagayapim@gmail.com>
2022-06-17 05:13:14 +0300

    indexedDb instance eklendi

---
+++ src/database.js
@@ -0,0 +1,33 @@
+const databaseName = 'foxql-app-storage'
+
+export default class {
+
+    constructor()
+    {
+        this.status = 'waiting'
+        this.request = null
+        this.ready = false
+    }
+
+    open({version, onupgradeneeded, onerror})
+    {
+        const indexedDb = window.indexedDB ||
+        window.mozIndexedDB ||
+        window.webkitIndexedDB ||
+        window.msIndexedDB ||
+        window.shimIndexedDB
+        
+        if(!indexedDb){
+            this.status = 'IndexedDB could not be found in this browser.'
+            return false
+        }
+
+        this.request = indexedDb.open(databaseName, version || 1)
+        this.request.onerror = onerror.bind(this)
+        this.request.onupgradeneeded = onupgradeneeded.bind(this)
+        this.request.onsuccess = (e)=> {
+            this.status = 'connected'
+            this.ready = true
+        }
+    }
+}
