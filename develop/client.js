352e34dba067cfc8f183d838ba2d87883b4732eb
boraozer <nagayapim@gmail.com>
2022-08-15 23:47:02 +0300

    uygulamalar için etiket kullanımı eklendi & sinyalleşme entegrasyonu tamamlandı

--- develop/client.js
+++ develop/client.js
@@ -7,7 +7,8 @@ const p2p = new network({
     maxCandidateCallTime: 2000, // ms
     bridgeServer: {
         host: 'http://127.0.0.1:1923'
-    }
+    },
+    dappAlias: 'test-dapp'
 })
 
 p2p.setMetaData({
